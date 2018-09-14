4850ec1d107b105124367c609b3a97369f26e575
Hyland <khyland@c02wk08qhtdd.mi.corp.rockfin.com>
2018-09-14 14:47:46 -0400

    Storing your code in repos so you can access it anywhere. what a concept.

---
+++ src/app/products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component {
+  selector: 'products',
+  template: '<h2>Products</h2>'
+}
+
+export class ProductsComponent {
+  // tslint:disable-next-line:no-trailing-whitespace
+
+}
